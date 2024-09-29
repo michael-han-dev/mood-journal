72b9bc78c886d8bbb0b1b6d2832ce59f208bcbd2
voyager14 <21mh124@queensu.ca>
2024-09-29 17:39:40 -0400

    added userflow to new user creation

--- mood-journal/app/page.tsx
+++ mood-journal/app/page.tsx
@@ -1,12 +1,19 @@
+import { auth } from '@clerk/nextjs/server'
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home(){
+  const {userId} = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
+
+
   return (<div className="w-screen h-screen bg-black flex justify-center items-center text-white">
     <div className="w-full max-w-[600px] mx-auto">
       <h1 className= "text-6xl mb-4">The best Journal App.</h1>
       <p className="text-2xl text-white/60 mb-4">A journaling app to help you track your mood</p>
       <div>
-        <Link href="/journal">
+        <Link href = {href}>
           <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started!</button>
         </Link>
       </div>
