1680356772bdff82e965e7a8f72bfb3d07b4aa59
voyager14 <21mh124@queensu.ca>
2024-10-01 22:34:11 -0400

    can add new pages and page link goes somewhere

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -1,3 +1,14 @@
+import { getUserFromClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+import { NextResponse } from "next/server"
+
 export const POST = async () => {
-    
+    const user = await getUserFromClerkID()
+    const entry = await prisma.journalEntry.create({
+        data: {
+            userId: user.id,
+            content: 'Write about your day here!',
+        },
+    })
+    return NextResponse.json({ data : entry })
 }
--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,8 +1,16 @@
 'use client'
 
+import { createNewEntry } from "@/utils/api"
+import { useRouter } from "next/navigation"
+
 const NewEntryCard = () => {
-    const handleOnClick = () => {}
-    
+    const router = useRouter()
+
+    const handleOnClick = async () => {
+        const data = await createNewEntry()
+        router.push(`/journal/${data.id}`)
+    }
+
     return (
         <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
           <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
