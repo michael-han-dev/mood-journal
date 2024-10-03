05881ca0248c8d54da7a8ff1e50d87417642649f
voyager14 <21mh124@queensu.ca>
2024-10-02 22:48:44 -0400

    display entry text

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,5 +1,26 @@
-const EntryPage = ({params}) => {
-    return <div>{params.id}</div>
+import Editor from "@/components/Editor"
+import { getUserFromClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getEntry = async (id) => {
+    const user = await getUserFromClerkID()
+    const entry = await prisma.journalEntry.findUnique({
+        where: { 
+            userId_id:{
+                userId: user.id,
+                id
+            } 
+        },
+    })
+    return entry
 }
+
+const EntryPage = async ({ params }) => {
+    const entry = await getEntry(params.id)
+    return <div>
+        <Editor entry={entry} />
+    </div>
+}
+
 
 export default EntryPage
