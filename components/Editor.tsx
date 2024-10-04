dbcee594e4c8fef0d1f98b7726d2bcdc4f49e72e
voyager14 <21mh124@queensu.ca>
2024-10-04 17:51:02 -0400

    npm autosave added for journal entries and sidebar for ai analysis added

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,11 +1,24 @@
 'use client'
+import { updateEntry } from '@/utils/api'
 import { useState } from 'react'
+import { useAutosave } from 'react-autosave'
 
 const Editor = ({entry}) => {
     const [value, setValue] = useState(entry.content)
+    const [isLoading, setIsLoading] = useState(false)
+    useAutosave({
+        data: value,
+        onSave: async(_value) => {
+            const updated = await updateEntry(entry.id, _value)
+            setIsLoading(false)
+        },
+    })
 
     return <div className="w-full h-full">
-        <textarea className="w-full h-full p-8 text- outline-none" value={value} onChange={(e) => setValue(e.target.value)} />       
+        {isLoading && <div>Loading...</div>}
+        <textarea className="w-full h-full p-8 text- outline-none" 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)} />       
     </div>
 }
 
