b95ddd02a1ae055ee6f4e3b0b239417736d03f86
voyager14 <21mh124@queensu.ca>
2024-10-15 21:41:35 -0400

    revalidation and menu navigation

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,6 +1,7 @@
 import { analyze } from "@/utils/ai";
 import { getUserFromClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const PATCH = async (request, context) => {
@@ -21,22 +22,20 @@ export const PATCH = async (request, context) => {
         });
         
         const analysis = await analyze(updatedEntry.content);
+        const updated = await prisma.analysis.upsert({
+            where: {
+                entryId: updatedEntry.id,
+            },
+            create: {
+                entryId: updatedEntry.id,
+                ...analysis,
+            },
+            update: analysis || {},
+        });
         
-        if (analysis) {
-            await prisma.analysis.upsert({
-                where: {
-                    entryId: updatedEntry.id,
-                },
-                create: {
-                    entryId: updatedEntry.id,
-                    ...analysis,
-                },
-                update: analysis || {},
-            });
-        }
 
-        
-        return NextResponse.json({ data: updatedEntry });
+
+        return NextResponse.json({ data: {...updatedEntry, analysis: updated} });
     } catch (error) {
         console.error("Error updating journal entry:", error);
         return NextResponse.error();
